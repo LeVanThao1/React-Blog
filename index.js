769859bccfff77bfe6d0137420d4eb36bda0022a
Your Name <you@example.com>
2020-08-24 16:49:43 +0700

    move private infomation

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key')
 
 mongoose.connect(
-        'mongodb+srv://thaolv210402:ta210402@cluster0.6z86x.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+        config.mongoURI, 
         {useNewUrlParser: true}
     ).then(() => console.log('connect db'))
     .catch((err) => console.log(err))
 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 app.listen(5000)
